627f8e0ab3cb46d067ba18131291ac60fb31c04c
nathanaelmemis <nathanaelmemis@gmail.com>
2025-05-09 20:22:44 +0800

    revised server start script

--- server/start_dev.ps1
+++ server/start_dev.ps1
@@ -1,9 +1,9 @@
 if (-Not (Test-Path -Path ".venv" -PathType Container)) {
     python -m venv .venv
+    .\.venv\Scripts\activate
+    pip install -r requirements.txt
+} else {
+    .\.venv\Scripts\activate
 }
-
-.\.venv\Scripts\activate
-
-pip install -r requirements.txt
 
 uvicorn main:app --reload --env-file .env.development --app-dir src
