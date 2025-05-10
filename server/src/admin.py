99f35542b6ccc7e5cc8901b8fc6a05068d99d6c8
nathanaelmemis <nathanaelmemis@gmail.com>
2025-05-10 12:14:49 +0800

    Added admin tokens

--- server/src/admin.py
+++ server/src/admin.py
@@ -1,27 +1,36 @@
+from datetime import datetime, timedelta
 from bson import ObjectId
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, Request, Response, status
+from fastapi.responses import JSONResponse
 from database import db
 
 from models.AdminCredentialsModel import AdminCredentialsModel
 import exception
 from schemas.AdminUserModel import AdminUserModel
-from auth import generate_access_token
+from auth import generate_token, hash
+import success
+
+ADMIN_ALLOWED_LOGIN_ATTEMPTS = 3
+ADMIN_ACCESS_TOKEN_EXP_MINS = 10
+ADMIN_REFRESH_TOKEN_EXP_MINS = 60
 
 admin_col = db.admin
 
 admin_router = APIRouter()
 
-@admin_router.post("/login")
-def login(body: AdminCredentialsModel):
+@admin_router.post("/admin/login")
+def admin_login(body: AdminCredentialsModel, res: Response):
     admin_user = AdminUserModel(**admin_col.find_one({ "email": body.email}))
 
     if (not admin_user):
         return exception.invalid_credentials
     
-    if (admin_user.login_attempts >= 3):
+    if (admin_user.login_attempts >= ADMIN_ALLOWED_LOGIN_ATTEMPTS):
         return exception.account_locked
     
-    if (not body.hash == admin_user.hash):
+    double_hash = hash(body.hash)
+    if (double_hash != admin_user.hash):
+        # increment login attempts when wrong password
         admin_col.update_one({ 
                 "_id": ObjectId(admin_user.id) 
             }, { 
@@ -29,4 +38,42 @@ def login(body: AdminCredentialsModel):
             })
         return exception.invalid_credentials
     
-    return generate_access_token(admin_user)
+    # reset login attempts when authenticated
+    admin_col.update_one({ 
+            "_id": ObjectId(admin_user.id) 
+        }, { 
+            "$set": { "login_attempts": 0 }
+        })
+    
+    access_token_exp = datetime.utcnow() + timedelta(minutes=ADMIN_ACCESS_TOKEN_EXP_MINS)
+    refresh_token_exp = datetime.utcnow() + timedelta(minutes=ADMIN_ACCESS_TOKEN_EXP_MINS)
+
+    access_token_data = admin_user.model_dump(exclude={"hash"})
+    refresh_token_data = { "_id": admin_user.id }
+    
+    access_token = generate_token(access_token_data, access_token_exp)
+    refresh_token = generate_token(refresh_token_data, refresh_token_exp)
+    
+    res.set_cookie(
+        "access_token", 
+        access_token,
+        httponly=True,
+        samesite="strict",
+        max_age=access_token_exp
+    )
+    res.set_cookie(
+        "refresh_token", 
+        refresh_token,
+        httponly=True,
+        samesite="strict",
+        max_age=refresh_token_exp
+    )
+
+    return { "message": "User authenticated" }
+
+@admin_router.get("/admin/logout")
+def admin_logout(res: Response):
+    res.delete_cookie(key="access_token")
+    res.delete_cookie(key="refresh_token")
+
+    return { "message": "User logged out" }
--- server/src/auth.py
+++ server/src/auth.py
@@ -1,16 +1,17 @@
 from datetime import datetime, timedelta
 from jose import jwt
 import os
+import hashlib
 
 from schemas.AdminUserModel import AdminUserModel
 
 ALGORITHM="HS256"
 
-def generate_access_token(data: AdminUserModel, expires_delta: timedelta = timedelta(hours=1)):
-    to_encode = data.model_dump()
-
-    expire = datetime.utcnow() + expires_delta
+def generate_token(data: dict, expire: datetime):
+    to_encode = data.copy()
     to_encode.update({ "exp": expire })
-
     encoded_jwt = jwt.encode(to_encode, os.environ["SECRET_KEY"], algorithm=ALGORITHM)
     return encoded_jwt
+
+def hash(text: str):
+    return hashlib.sha256(text.encode()).hexdigest()
